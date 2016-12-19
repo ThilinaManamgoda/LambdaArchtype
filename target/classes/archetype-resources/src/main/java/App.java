18ade7836702681a9865d5d31592d659984941e2
ThilinaManamgoda <maanafunedu@gmail.com>
2016-12-19 10:42:46 +0530

    first commit

---
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,15 @@
+
+
+import org.wso2.core.Context;
+import org.wso2.core.EventDeserializersManager;
+import org.wso2.core.RequestHandler;
+import org.wso2.core.models.APIM_Create;
+
+public class App implements RequestHandler {
+
+    public Object handleRequest(Context context, EventDeserializersManager eventDeserializersManager, Object input) {
+
+
+        return null;
+    }
+}
