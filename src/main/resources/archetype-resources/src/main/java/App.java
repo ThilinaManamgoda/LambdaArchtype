b312987bcb540f0063b90c011ea7068dd3cc5acc
ThilinaManamgoda <maanafunedu@gmail.com>
2016-12-20 18:24:40 +0530

    logic is modified

--- src/main/resources/archetype-resources/src/main/java/App.java
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -1,15 +1,6 @@
 
 
-import org.wso2.core.Context;
-import org.wso2.core.EventDeserializersManager;
-import org.wso2.core.RequestHandler;
-import org.wso2.core.models.APIM_Create;
 
-public class App implements RequestHandler {
+public class App {
 
-    public Object handleRequest(Context context, EventDeserializersManager eventDeserializersManager, Object input) {
-
-
-        return null;
-    }
 }
